b29eb8dbfe87b22ac1293d1e661660543e6c0615
HICHAMOH12 <bennajehh@gmail.com>
2018-01-23 21:50:28 -0500

    Finalisation des Methode et ajout de donnees de test

--- LOG330_Laboratoire01/src/Calcul/Variance/InterfaceCalcul.java
+++ LOG330_Laboratoire01/src/Calcul/Variance/InterfaceCalcul.java
@@ -1,5 +1,10 @@
 package Calcul.Variance;
 
+/**
+ * @author HICHAM
+ * Description : cette interface permet de creer les methode pour calculer .
+ */
+
 public interface InterfaceCalcul {
 	
 	public double calculeDeMoyenne();

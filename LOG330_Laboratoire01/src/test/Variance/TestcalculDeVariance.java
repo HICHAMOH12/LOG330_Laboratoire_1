1f84db6eb112a5d29e4d308446c019ae42adfa24
HICHAM <hicham.bennajeh.1@ens.etsmtl.ca>
2018-02-06 23:22:34 -0500

    ajouter test

--- LOG330_Laboratoire01/src/test/Variance/TestcalculDeVariance.java
+++ LOG330_Laboratoire01/src/test/Variance/TestcalculDeVariance.java
@@ -1,19 +1,25 @@
 package test.Variance;
 
+import java.util.ArrayList;
+
+import org.junit.Test;
+
 public class TestcalculDeVariance {
+	
+	private ArrayList<Integer> ValeurTest ;
 	
 	
 	   public class TestcalculEcartType {
 
-		
+		@Test
 		public void TestlmiteInferieure() {
 			
 		}
-		
+		@Test
 	    public void TestlmiteSuperieure() {
 			
 		}
-	    
+	    @Test
 	    public void TestValeurInvalide() {
 			
 		}
--- LOG330_Laboratoire01/src/test/Variance/TestcalculEcartType.java
+++ LOG330_Laboratoire01/src/test/Variance/TestcalculEcartType.java
@@ -1,16 +1,22 @@
 package test.Variance;
 
-public class TestcalculEcartType {
+import java.util.ArrayList;
 
+import org.junit.Test;
+
+public class TestcalculEcartType {
 	
+	private ArrayList<Integer> ValeurTest ;
+
+	@Test
 	public void TestlmiteInferieure() {
 		
 	}
-	
+	@Test
     public void TestlmiteSuperieure() {
 		
 	}
-    
+    @Test
     public void TestValeurInvalide() {
 		
 	}
